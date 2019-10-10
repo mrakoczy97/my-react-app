23fade01b23bbc873a2fbfa3d2b348e15f25289f
Mariusz Rakoczy <rakoczma@company.schiedel.net>
2019-10-10 09:04:59 +0200

    Updates

---
+++ precache-manifest.4b04e3e86e0f80b35451b88f9696b703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b5e13fadda3b4c8216ff36fcf7171f",
+    "url": "/complaintssystem/index.html"
+  },
+  {
+    "revision": "5e8e744fb885e67083b2",
+    "url": "/complaintssystem/static/css/main.809cde28.chunk.css"
+  },
+  {
+    "revision": "c2a0fc620c3ac662d591",
+    "url": "/complaintssystem/static/js/2.8c28ae90.chunk.js"
+  },
+  {
+    "revision": "5e8e744fb885e67083b2",
+    "url": "/complaintssystem/static/js/main.5b837fd3.chunk.js"
+  },
+  {
+    "revision": "ce0be2ef54dfad1838a9",
+    "url": "/complaintssystem/static/js/runtime~main.b324071f.js"
+  }
+]);
